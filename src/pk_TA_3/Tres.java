a8c6a091e4f52369073b85bb48cba68de036e29e
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-10 16:11:32 +0200

    añadido punto 3

---
+++ src/pk_TA_3/Tres.java
@@ -0,0 +1,48 @@
+package pk_TA_3;
+
+import java.util.Scanner;
+
+public class Tres {
+
+	/*
+	 * Comprobar si el numero es primo
+	 */
+	public static void main(String[] args) {
+		
+		int numeroCheck = 0;
+		
+		// Input la cantidad total de numeros por input
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("¿Es número primo?");
+		System.out.println("Introduce el numero:");
+		numeroCheck = scnr.nextInt();
+		
+		if(isPrime(numeroCheck)) {
+			System.out.println("El número es primo.");
+		} else {
+			System.out.println("El número no es primo");
+		}
+
+	}
+
+	/*
+	 * Prueba si el numero recibido es primo o no
+	 * source: https://www.javatpoint.com
+	 */
+	public static boolean isPrime(int number) {
+
+		if (number <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i < Math.sqrt(number); i++) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+}
